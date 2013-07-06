6ceb777e2cb36f9972642f299b3b3900aa2f197f
GierAffe <daniel.ruefenacht@gmail.com>
2013-07-06 22:56:02 +0200

    changed commentary

--- common/gieraffe/bdc/gui/GuiHandler.java
+++ common/gieraffe/bdc/gui/GuiHandler.java
@@ -8,7 +8,7 @@ import net.minecraft.world.World;
 import cpw.mods.fml.common.network.IGuiHandler;
 
 public class GuiHandler implements IGuiHandler {
-    //returns an instance of the Container you made earlier
+    //returns an instance of the Container
     @Override
     public Object getServerGuiElement(int id, EntityPlayer player, World world, int x, int y, int z) {
     	TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
@@ -18,7 +18,7 @@ public class GuiHandler implements IGuiHandler {
     	return null;
     }
 
-    //returns an instance of the Gui you made earlier
+    //returns an instance of the Gui
     @Override
     public Object getClientGuiElement(int id, EntityPlayer player, World world, int x, int y, int z) {
     	TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
