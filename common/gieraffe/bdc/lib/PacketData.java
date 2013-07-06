497bc30fcff77f101905d6f414ca94c7724caeaa
GierAffe <daniel.ruefenacht@gmail.com>
2013-07-06 22:49:36 +0200

    renamed from package to packet overloaded constructor for CustomBDCPacket for easier packet handling

---
+++ common/gieraffe/bdc/lib/PacketData.java
@@ -0,0 +1,11 @@
+package gieraffe.bdc.lib;
+
+public class PacketData {
+	/* power button in the detector gui clicked */
+	public static final int BUTTON_POWER_CLICKED = 100;
+	
+	/* client message for changing power state on gui*/
+	public static final int CHANGE_POWER_STATE = 101;
+	public static final int POWER_STATE_ON = 102;
+	public static final int POWER_STATE_OFF = 103;
+}
