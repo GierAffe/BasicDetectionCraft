1a549ca866e1b49398c9961c7f956a6d2f5738ae
GierAffe <daniel.ruefenacht@gmail.com>
2013-07-06 02:49:25 +0200

    major change now it reads out packages sent to the server and gets the tileentity for propper message forwarding

--- common/gieraffe/bdc/network/PacketHandler.java
+++ common/gieraffe/bdc/network/PacketHandler.java
@@ -1,16 +1,23 @@
 package gieraffe.bdc.network;
 
+import gieraffe.bdc.lib.BlockIDs;
 import gieraffe.bdc.lib.Channels;
+import gieraffe.bdc.tile.TileDetector;
 
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteStreams;
 
 import net.minecraft.network.INetworkManager;
 import net.minecraft.network.packet.Packet250CustomPayload;
+import net.minecraft.src.ModLoader;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 import cpw.mods.fml.common.network.IPacketHandler;
 import cpw.mods.fml.common.network.Player;
 
 public class PacketHandler implements IPacketHandler {
+	
+	private World world = ModLoader.getMinecraftInstance().theWorld;
 
 	@Override
 	public void onPacketData(INetworkManager manager, Packet250CustomPayload packet, Player player) {
@@ -31,21 +38,34 @@ public class PacketHandler implements IPacketHandler {
         case 0:
         	break;
         }
+        
 	}
 	
 	private void handleServerPacket (INetworkManager manager, Packet250CustomPayload packet, Player player) {
 		ByteArrayDataInput data = ByteStreams.newDataInput(packet.data);
-		int x1 = data.readInt();
-		int x2 = data.readInt();
 		
-		data = ByteStreams.newDataInput(packet.data);
-		int y1 = data.readInt();
-		//data.readInt()
+		int[] values = new int[5];		// first 5 variables for identification
+		for (int i = 0; i < 5; i++) {
+			values[i] = data.readInt();
+		}
 		
-        //switch (0) {
-        //case 0:
-        	
-        //	break;
-        //}
+		// read out the message in the data
+		int[] message = new int[data.readInt()];
+		for (int i = 0; i < message.length; i++) {
+			message[0] = data.readInt();
+		}
+		
+		TileEntity tile = world.getBlockTileEntity(values[1], values[2], values[3]); 	//values = x,y,z
+		
+		if (values[0] == BlockIDs.BLOCK_DETECTOR) {										//value[0] = tile identification
+			((TileDetector) tile).buttonPowerSwitchClicked(message);
+		}
 	}
 }
+
+
+
+
+
+
+
